cc3b8b35ff558d807bdd7305b33f1516d289936b
Bastian Hofmann <mail@bastianhofmann.de>
2024-08-30 14:30:06 +0200

    Migrate QdrantCloudRegions CRD

---
+++ api/v1/region_types.go
@@ -0,0 +1,137 @@
+package v1
+
+import (
+	helmapi "github.com/fluxcd/helm-controller/api/v2beta2"
+	srcapi "github.com/fluxcd/source-controller/api/v1beta2"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	KindQdrantCloudRegion     = "QdrantCloudRegion"
+	ResourceQdrantCloudRegion = "qdrantcloudregions"
+)
+
+// QdrantCloudRegionSpec defines the desired state of QdrantCloudRegion
+type QdrantCloudRegionSpec struct {
+	// Id specifies the unique identifier of the region
+	Id string `json:"id,omitempty"`
+	// HelmRepositories specifies the list of helm repositories to be created to the region
+	// +optional
+	HelmRepositories []HelmRepository `json:"helmRepositories,omitempty"`
+	// HelmReleases specifies the list of helm releases to be created to the region
+	// +optional
+	HelmReleases []HelmRelease `json:"helmReleases,omitempty"`
+}
+
+type HelmRepository struct {
+	// MarkedForDeletionAt specifies the time when the helm repository was marked for deletion
+	// +optional
+	MarkedForDeletionAt *string `json:"markedForDeletionAt,omitempty"`
+	// Object specifies the helm repository object
+	// +kubebuilder:validation:EmbeddedResource
+	Object *srcapi.HelmRepository `json:"object,omitempty"`
+}
+
+type HelmRelease struct {
+	// MarkedForDeletionAt specifies the time when the helm release was marked for deletion
+	// +optional
+	MarkedForDeletionAt *string `json:"markedForDeletionAt,omitempty"`
+	// Object specifies the helm release object
+	// +kubebuilder:validation:EmbeddedResource
+	Object *helmapi.HelmRelease `json:"object,omitempty"`
+}
+
+type RegionPhase string
+
+const (
+	RegionPhaseReady    RegionPhase = "Ready"
+	RegionPhaseNotReady RegionPhase = "NotReady"
+	FailedToSync        RegionPhase = "FailedToSync"
+)
+
+type QdrantCloudRegionStatus struct {
+	// Phase specifies the current phase of the region
+	// +optional
+	Phase RegionPhase `json:"phase,omitempty"`
+	// KubernetesVersion specifies the version of the Kubernetes cluster
+	// +optional
+	KubernetesVersion string `json:"k8sVersion,omitempty"`
+	// NumberOfNodes specifies the number of nodes in the Kubernetes cluster
+	// +optional
+	NumberOfNodes int `json:"numberOfNodes,omitempty"`
+	// Capabilities specifies the capabilities of the Kubernetes cluster
+	// +optional
+	Capabilities *RegionCapabilities `json:"capabilities,omitempty"`
+	// HelmRepositories specifies the status of the helm repositories
+	// +optional
+	HelmRepositories []ComponentStatus `json:"helmRepositories,omitempty"`
+	// HelmReleases specifies the status of the helm releases
+	// +optional
+	HelmReleases []ComponentStatus `json:"helmReleases,omitempty"`
+	// Message specifies the info explaining the current phase of the region
+	// +optional
+	Message string `json:"message,omitempty"`
+}
+
+type RegionCapabilities struct {
+	// VolumeSnapshot specifies whether the Kubernetes cluster supports volume snapshot
+	// +optional
+	VolumeSnapshot *bool `json:"volumeSnapshot,omitempty"`
+	// VolumeExpansion specifies whether the Kubernetes cluster supports volume expansion
+	// +optional
+	VolumeExpansion *bool `json:"volumeExpansion,omitempty"`
+}
+
+type ComponentPhase string
+
+const (
+	ComponentPhaseReady    ComponentPhase = "Ready"
+	ComponentPhaseNotReady ComponentPhase = "NotReady"
+	ComponentPhaseUnknown  ComponentPhase = "Unknown"
+	ComponentPhaseNotFound ComponentPhase = "NotFound"
+)
+
+type ComponentStatus struct {
+	// Name specifies the name of the component
+	Name string `json:"name"`
+	// Namespace specifies the namespace of the component
+	Namespace string `json:"namespace,omitempty"`
+	// Version specifies the version of the component
+	// +optional
+	Version string `json:"version,omitempty"`
+	// Phase specifies the current phase of the component
+	Phase ComponentPhase `json:"phase,omitempty"`
+	// Message specifies the info explaining the current phase of the component
+	// +optional
+	Message string `json:"message,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+// +kubebuilder:pruning:PreserveUnknownFields
+// +kubebuilder:resource:path=qdrantcloudregions,scope=Cluster,singular=qdrantcloudregion,shortName=region;regions
+// +kubebuilder:printcolumn:name="K8s Version",type=string,JSONPath=`.status.k8sVersion`
+// +kubebuilder:printcolumn:name="Phase",type=string,JSONPath=`.status.phase`
+// +kubebuilder:printcolumn:name="Age",type=date,JSONPath=`.metadata.creationTimestamp`
+
+// QdrantCloudRegion is the Schema for the qdrantcloudregions API
+type QdrantCloudRegion struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   QdrantCloudRegionSpec   `json:"spec,omitempty"`
+	Status QdrantCloudRegionStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// QdrantCloudRegionList contains a list of QdrantCloudRegion
+type QdrantCloudRegionList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []QdrantCloudRegion `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&QdrantCloudRegion{}, &QdrantCloudRegionList{})
+}
--- go.mod
+++ go.mod
@@ -3,12 +3,17 @@ module github.com/qdrant/operator-api
 go 1.22.2
 
 require (
+	github.com/fluxcd/helm-controller/api v0.37.4
+	github.com/fluxcd/source-controller/api v1.2.5
 	k8s.io/api v0.31.0
 	k8s.io/apimachinery v0.31.0
 	sigs.k8s.io/controller-runtime v0.19.0
 )
 
 require (
+	github.com/fluxcd/pkg/apis/acl v0.1.0 // indirect
+	github.com/fluxcd/pkg/apis/kustomize v1.3.0 // indirect
+	github.com/fluxcd/pkg/apis/meta v1.3.0 // indirect
 	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
@@ -21,8 +26,10 @@ require (
 	golang.org/x/text v0.16.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	k8s.io/apiextensions-apiserver v0.31.0 // indirect
 	k8s.io/klog/v2 v2.130.1 // indirect
 	k8s.io/utils v0.0.0-20240711033017-18e509b52bc8 // indirect
 	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.4.1 // indirect
+	sigs.k8s.io/yaml v1.4.0 // indirect
 )
