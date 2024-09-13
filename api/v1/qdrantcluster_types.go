90f9a6e4fca506459fa5a3512ab93692b2430d20
Robert-Stam <robert.stam@qdrant.com>
2024-09-13 17:19:43 +0200

    switched order

--- api/v1/qdrantcluster_types.go
+++ api/v1/qdrantcluster_types.go
@@ -16,19 +16,19 @@ const (
 
 const (
 	// RestartedAtAnnotationKey is the annotation key to trigger a restart.
+	// The annotation should be placed on the QdrantCluster instance.
 	// The value should be a [RFC3339 formatted] date.
 	// If the value is updated it will retrigger the restart.
 	// The operator will not remove the annotation once restarted.
 	// For historical reasons the key doesn't start with `operator.qdrant.com/`
-	// The annotation should be placed on the QdrantCluster instance.
 	RestartedAtAnnotationKey = "restartedAt"
 	// RecreateNodeAnnotationKey is the annotation key to recreate a certain node.
+	// The annotation should be placed on the QdrantCluster instance.
+	// Multiple annotations can be added, the operator will handle them all.
 	// The value should contain the node index to be replaced (e.g. "3").
 	// The node index should be one of the values specified in .Status.AvailableNodeIndexes.
 	// Once the operator has replaced the node it will remove the annotation.
 	// If a none existing node index is provided, the operator will remove the annotation (with a warning in the log).
-	// The annotation should be placed on the QdrantCluster instance.
-	// Multiple annotations can be added, the operator will handle them all.
 	RecreateNodeAnnotationKey = "operator.qdrant.com/recreate-node"
 )
 
