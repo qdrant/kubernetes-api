64b46b2a29b347285e9639c7dc644eddbd01ece7
Maksim Dorokhin <maksim.dorokhin@qdrant.com>
2025-03-27 19:20:20 +0100

    switch to observedGeneration to track Entity Status updates

--- api/v1/qdrantentity_types.go
+++ api/v1/qdrantentity_types.go
@@ -74,8 +74,8 @@ type QdrantEntityStatus struct {
 	Phase EntityPhase `json:"phase,omitempty"`
 	// Result is the last result from the invocation to a manager
 	Result QdrantEntityStatusResult `json:"result,omitempty"`
-	// Timestamp when the status was last updated.
-	LastUpdatedAt metav1.MicroTime `json:"lastUpdatedAt,omitempty"`
+	// ObservedGeneration is the entity.Generation the Status is related to
+	ObservedGeneration int64 `json:"observedGeneration,omitempty"`
 }
 
 // EntityResult is the last result from the invocation to a manager
